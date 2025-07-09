b43db9b223e5c6f179d52ba7ddeadc27ca692d21
faltemi <michael.falter5@gmail.com>
2025-07-09 11:16:37 -0400

    basic implementation

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # hashtable
 C hashtable testing for use in my interpreter project
+Followed this tutorial: https://www.youtube.com/watch?app=desktop&v=2Ti5yvumFTU&t=782s
--- hashtable.c
+++ hashtable.c
@@ -4,18 +4,83 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+#define TABLE_SIZE 34
+
 typedef struct ExampleObj{
     char* name;
     int val;
 }ExampleObj;
 
-unsigned int hash(char* name){
-    return 0;
+ExampleObj* hash_table[TABLE_SIZE];
+
+unsigned int hash(const char* name){
+    int len = strlen(name);
+    unsigned int hashVal = 0;
+    for(int i = 0; i < len; i++){
+        hashVal += name[i];
+        hashVal *= name[i];
+        hashVal %= TABLE_SIZE;
+    }
+    return hashVal;
+}
+
+void init_hash_table(){
+    for(int i = 0; i < TABLE_SIZE; i++){
+        hash_table[i] = NULL;
+    }
+}
+
+void print_table(){
+    for(int i = 0; i < TABLE_SIZE; i++){
+        if(hash_table[i]){
+            printf("\t%i\t%s\n", i, hash_table[i]->name);
+        }
+        else{
+           printf("\t%i\t---\n", i); 
+        }
+    }
+}
+
+bool hash_table_insert(ExampleObj* v){
+    if(!v) return false;
+    int idx = hash(v->name);
+    if(hash_table[idx] != NULL){
+        printf("Collision!\n");
+        return false;
+    }
+    hash_table[idx] = v;
+    return true;
+}
+
+ExampleObj* lookup_hash(const char* name){
+    int idx = hash(name);
+    if(hash_table[idx] && strcmp(hash_table[idx]->name, name) == 0) return hash_table[idx];
+    return NULL;
+}
+
+ExampleObj* delete_hash(const char* name){
+    int idx = hash(name);
+    if(hash_table[idx] && strcmp(hash_table[idx]->name, name) == 0){
+        ExampleObj* tmp = hash_table[idx];
+        hash_table[idx] = NULL;
+        return tmp;
+    }
+    return NULL;
 }
 
 int main(){
-    printf("Bob Hashed: %d\n", hash("Bob"));
-    printf("Ron Hashed: %d\n", hash("Ron"));
-    printf("Tracy Hashed: %d\n", hash("Tracy"));
+    init_hash_table();
+    ExampleObj bob = {.name = "Bob", .val = 13};
+    ExampleObj jon = {.name = "Jon", .val = 23};
+    ExampleObj tracy = {.name = "Tracy", .val = 43};
+    ExampleObj kate = {.name = "Kate", .val = 32};
+    hash_table_insert(&bob);
+    hash_table_insert(&jon);
+    hash_table_insert(&tracy);
+    hash_table_insert(&kate);
+    print_table();
+    ExampleObj* tmp = lookup_hash("Bob");
+    printf("Found %s\n", tmp->name);
+    
     return 0;
 }
